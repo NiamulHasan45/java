c8bc215cc1d681e4647b0409a5fd94bf1c34e128
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-01-29 15:49:07 +0600

    function

---
+++ function.js
@@ -0,0 +1,30 @@
+// function classRoom(){
+//     console.log("There are fifty two students in our classroom.")
+// }
+// classRoom();
+// classRoom();
+
+// function totalExpenditure(taka1, taka2){
+//     var total = taka1+taka2;
+//     console.log("Total khoroch hoise", total);
+// }
+
+// var taka1, taka2;
+// totalExpenditure(234,456);
+
+// function totalExpenditure(taka1, taka2){
+//     var total = taka1+taka2;
+//     console.log("Total khoroch hoise", total);
+// }
+
+// var taka1, taka2;
+// totalExpenditure('Hello', 'Honey');
+
+function totalMarks(sub1, sub2, sub3){
+    var total= sub1+sub2+sub3;
+    return total;
+}
+
+var Bangla=12, English=23, Math= 67;
+total=totalMarks(Bangla, English, Math);
+console.log(total);
